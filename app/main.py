728cf48422e210d392068a3df0a2e302f5294a55
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-05-21 17:28:06 +0100

    Added / and /inventory endpoints to main.py

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-
+import psycopg
 
 app = FastAPI()
+
+@app.get("/")
+async def hello():
+    return {"message": "Congrats, you  launched a server"}
 
 
 @app.get("/ping")
 async def ping():
-    return {"message": "Hello Worlds"}
+    return {"message": "Ping endpoint functioning"}
+
+app.put("/inventory")
+def new_item():
+    pass
