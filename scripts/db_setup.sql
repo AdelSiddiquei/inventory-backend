2168a3fa64e848db15f89ef8e915f284be846b4b
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-05-21 12:14:52 +0100

    Setup inventory and transactions tables in database

---
+++ scripts/db_setup.sql
@@ -0,0 +1,20 @@
+-- Create table
+CREATE TABLE inventory (
+    item text NOT NULL,
+    description text,
+    price float NOT NULL,
+	PRIMARY KEY (item)
+);
+
+-- Primary & Foreign Keys
+CREATE TABLE transactions (
+    transaction_id uuid DEFAULT gen_random_uuid() NOT NULL,
+	item text NOT NULL,
+    quantity int NOT NULL,
+	PRIMARY KEY (transaction_id),
+	FOREIGN KEY (item) REFERENCES inventory(item)
+);
+
+-- Insert
+INSERT INTO inventory (item, description, price)
+VALUES ('T-shirt', 'Black & white stripy t-shirt.', 12.99), ('Hoodie', 'Bright orange hoodie', 20.99)
