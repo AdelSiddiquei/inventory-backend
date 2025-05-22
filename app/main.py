5f312e0528b71bb1cf6e483e983ea330ea169ac5
Adel Siddiquei <adelsiddiquei@gmail.com>
2025-05-22 16:51:49 +0100

    implementing endpoint - unfinished

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
-from utils import DBTools
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import os
+from fastapi import FastAPI, HTTPException, Request
+import psycopg
+from pydantic import BaseModel
 
 app = FastAPI()
+
 
 @app.get("/")
 async def hello():
@@ -12,11 +19,185 @@ async def hello():
 async def ping():
     return {"message": "Ping endpoint functioning"}
 
-@app.put("/inventory")
-def new_stock():
-    pass
+
+class InventoryRequest(BaseModel):
+    item: str
+    description: str
+    price: float
+
+class TransactionsRequest(BaseModel):
+    item: str
+    quantity: int
+
+
+
+# Inventory endpoints
+@app.post("/inventory/")
+def new_item(body: InventoryRequest):
+    try:
+        with psycopg.connect(
+            dbname=os.getenv("POSTGRES_DBNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+        ) as conn:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                query = f"INSERT INTO inventory (item, description, price) VALUES ('{body.item}', '{body.description}', {body.price})"
+                cursor.execute(query)
+            return {
+                "data": {
+                    "message": "Added successfully",
+                    "item": body.item,
+                    "description": body.description,
+                    "price": body.price,
+                }
+            }
+    except Exception as e:
+        return HTTPException(500, "There was an error, try again later.")
+
 
 @app.get("/inventory")
-def get_inventory():
-    dbsession = DBTools()
-    dbsession.get_table_as_array('inventory')
+def get_items():
+    try:
+        with psycopg.connect(
+            dbname=os.getenv("POSTGRES_DBNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+        ) as conn:
+            with conn.cursor() as cursor:
+                query = "SELECT * FROM inventory;"
+                cursor.execute(query)
+                data = cursor.fetchall()
+
+        return {"data": {"inventory": data}}
+    except Exception as e:
+        return HTTPException(500, "There was an error, try again later.")
+
+
+@app.delete("/inventory/{item}")
+def remove_item(item):
+    try:
+        with psycopg.connect(
+            dbname=os.getenv("POSTGRES_DBNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            autocommit=True,
+        ) as conn:
+            with conn.cursor() as cursor:
+                query = f"DELETE FROM inventory WHERE item = '{item}'"
+                cursor.execute(query)
+
+        return {
+            "data": {
+                "message": "Delete successful",
+                "item": item,
+                "description": description,
+                "price": price,
+            }
+        }
+    except Exception as e:
+        return HTTPException(500, "There was an error, try again later.")
+
+
+@app.patch("/inventory")
+def update_item(body: InventoryRequest):
+    try:
+        with psycopg.connect(
+            dbname=os.getenv("POSTGRES_DBNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            autocommit=True,
+        ) as conn:
+            with conn.cursor() as cursor:
+                query = f"UPDATE inventory SET description = '{body.description}', price = {body.price} WHERE item = '{body.item}';"
+                cursor.execute(query)
+
+        return {"data": {"message": "Update successful", "item": body.item, "description": body.description, "price": body.price}}
+    except Exception as e:
+        return HTTPException(500, "There was an error, try again later.")
+
+
+# Transactions endpoints
+@app.post("/transactions/")
+def new_order(body: TransactionsRequest):
+    try:
+        with psycopg.connect(
+            dbname=os.getenv("POSTGRES_DBNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+        ) as conn:
+            conn.autocommit = True
+            with conn.cursor() as cursor:
+                query = f"INSERT INTO transactions (item, quantity) VALUES ('{body.item}', {body.quantity})"
+                cursor.execute(query)
+            return {
+                "data": {
+                    "message": "Added successfully",
+                    "item": body.item,
+                    "quantity": body.quantity,
+                }
+            }
+    except Exception as e:
+        return HTTPException(500, "There was an error, try again later.")
+
+
+@app.get("/transactions")
+def get_orders(table_name: str):
+    try:
+        with psycopg.connect(
+            dbname=os.getenv("POSTGRES_DBNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+        ) as conn:
+            with conn.cursor() as cursor:
+                query = "SELECT * FROM inventory;"
+                cursor.execute(query)
+                data = cursor.fetchall()
+
+        return {"data": {"inventory": data}}
+    except Exception as e:
+        return HTTPException(500, "There was an error, try again later.")
+
+
+@app.delete("/transactions/{transaction_id}")
+def remove_order(transaction_id):
+    try:
+        with psycopg.connect(
+            dbname=os.getenv("POSTGRES_DBNAME"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            autocommit=True,
+        ) as conn:
+            with conn.cursor() as cursor:
+                query = f"DELETE FROM transactions WHERE transaction_id = '{transaction_id}'"
+                cursor.execute(query)
+
+        return {
+            "data": {
+                "message": "Delete successful",
+                "item": item,
+                "description": description,
+                "price": price,
+            }
+        }
+    except Exception as e:
+        return HTTPException(500, "There was an error, try again later.")
+
+
+# @app.put("/transactions")
+# def update_order():
+#     pass
